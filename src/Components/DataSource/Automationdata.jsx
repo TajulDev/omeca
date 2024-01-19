64a6c4f121bf309e454f188cf26229b05bac544c
nipaaa <akternipa941@gmail.com>
2024-01-19 18:20:33 +0600

    update

--- src/Components/DataSource/Automationdata.jsx
+++ src/Components/DataSource/Automationdata.jsx
@@ -2,8 +2,10 @@ import ai from "../../assets/ai.png";
 import bot from "../../assets/bot.png";
 import human from "../../assets/human.png";
 import api from "../../assets/api.png";
+import { useState } from "react";
 
 const Automationdata = () => {
+  const [active, setActive] = useState(null);
   const details = [
     {
       pic: ai,
@@ -26,6 +28,10 @@ const Automationdata = () => {
       amount: 20,
     },
   ];
+
+  const handleCardClick = (index) => {
+    setActive(index === active ? null : index);
+  };
   return (
     <div>
       <p className="text-[34px] font-bold text-primary mb-[25px]">
@@ -37,8 +43,11 @@ const Automationdata = () => {
       <div className="grid grid-cols-3 gap-[14px]">
         {details.map((data, index) => (
           <div
-            className="shadow-prompts border border-transparent hover:border-royalBlue transition-all duration-300  rounded-[10px] p-5 flex flex-col justify-center items-center w-full"
+            className={`shadow-prompts border ${
+              index === active ? "border-royalBlue" : "border-transparent"
+            } hover:border-royalBlue transition-all duration-300  rounded-[10px] p-5 flex flex-col justify-center items-center w-full`}
             key={index}
+            onClick={() => handleCardClick(index)}
           >
             <img className="mb-2" src={data.pic} alt="" />
             <p className="text-[18px] font-normal">
